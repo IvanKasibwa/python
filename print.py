a7bf3cbe87a656f5e384786e7b8b917c7a8ff41d
Ivan Kasibwa <ivanagesa@gmail.com>
2023-08-03 21:31:14 +0300

    first commit

---
+++ print.py
@@ -0,0 +1,5 @@
+# this is a comment
+print ('My name is Ivan')
+print('I am 33 years old')
+print ('I expect to learn new skills as a dev')
+
---
+++ variables.py
@@ -0,0 +1,24 @@
+# Variables to hold two things that you love
+love1 = "Music"
+love2 = "Coding"
+
+# Variables to hold two reasons why you want to learn programming
+reason1 = "To create useful applications"
+reason2 = "To solve complex problems"
+
+# Variables to hold two things that give you motivation
+motivation1 = "Self-improvement"
+motivation2 = "Making a positive impact"
+
+# Printing the values of the variables
+print("Things I love:")
+print(love1)
+print(love2)
+
+print("\nReasons why I want to learn programming:")
+print(reason1)
+print(reason2)
+
+print("\nThings that give me motivation:")
+print(motivation1)
+print(motivation2)
